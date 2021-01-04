9c3b993a576561f27250218097cb8e0a71e5d668
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2021-01-03 21:42:23 -0700

    added dashboard and login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React, { useState } from "react"
+import Dashboard from "./Components/Dashboard"
+import Login from "./Components/Login"
+import "./CSS/App.css"
 
-function App() {
+function App () {
+  let whichPage = "Dashboard"
+
+  const renderPage = () => {
+    switch (whichPage){
+      case "Dashboard":
+        return <Dashboard/>
+      case "Login":
+        return <Login/>
+      default:
+        return <Login/>
+    }
+  }
+
+  console.log("whichPage is ")
+  console.log(whichPage)
+
   return (
     <div className="App">
-      yoyo
+      {renderPage()}
     </div>
   );
 }
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../CSS/landingPage.css"
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <div className="helpUser">
+                Help a user
+            </div>
+            <div className="lookResidence">
+                Look for residence
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
